cad0291e56d4cab816079b2eefa9c80cb903846b
Jan Giacomelli <jan.giacomelli@algoritmik.si>
2017-10-16 19:26:33 +0200

    Loops added

---
+++ loops.py
@@ -0,0 +1,15 @@
+# break_loop = False
+
+# while not break_loop:
+#     x = int(raw_input("Enter the value for x: "))
+#     y = int(raw_input("Enter the value for y: "))
+#
+#     if x % y == 3:
+#         print('Ost = 3')
+#     elif x % y == 0:
+#         break_loop = True
+
+x = 12
+
+for i in range(0, x):
+    print(i)
